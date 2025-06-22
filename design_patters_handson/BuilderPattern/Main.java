a885f7221315e1d77049df28ab505d2c997e8c1e
Priyanshu Sharma <priyan90204@gmail.com>
2025-06-22 11:54:59 +0530

    Update Builder Pattern

--- design_patters_handson/BuilderPattern/Main.java
+++ design_patters_handson/BuilderPattern/Main.java
@@ -1,10 +1,9 @@
 package DesignPatterns.BuilderPattern;
 
-// File: src/Main.java
-// File: src/Main.java
+
 public class Main {
     public static void main(String[] args) {
-        // Create first computer
+
         Computer computer1 = new Computer.Builder()
                 .setCpu("Intel i5")
                 .setRam("8GB")
@@ -13,11 +12,11 @@ public class Main {
 
         System.out.println(computer1);
 
-        // Create another computer with different config
+
         Computer computer2 = new Computer.Builder()
                 .setCpu("AMD Ryzen 5")
                 .setRam("16GB")
-                .build(); // No storage provided
+                .build();
 
         System.out.println(computer2);
     }
