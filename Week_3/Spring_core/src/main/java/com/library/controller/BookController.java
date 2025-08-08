994aeab5223d4f78db63707b59bea61d7dd2d23a
Priyanshu Sharma <priyan90204@gmail.com>
2025-08-08 20:42:05 +0530

    Updated Code

--- Week_3/Spring_core/src/main/java/com/library/controller/BookController.java
+++ Week_3/Spring_core/src/main/java/com/library/controller/BookController.java
@@ -1,6 +1,6 @@
-package com.library.controller;
+package Week_3.Spring_core.src.main.java.com.library.controller;
 
-import com.library.model.Book;
+import com.library.model.Book.controller;
 import com.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
